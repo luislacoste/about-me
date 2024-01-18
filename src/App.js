a229fdc294dfccc91eb79a2c90b9bf4a6d4d6cbe
Luis <luis.lacoste99@gmail.com>
2024-01-18 20:33:17 -0300

    about me page starting to take form. really bad day :(

--- src/App.js
+++ src/App.js
@@ -1,94 +1,31 @@
 import React, { useState } from "react";
 import "./App.css";
+import aboutMe from "./text/aboutMe.js";
 
-export default function TicTacToe() {
-  const [turn, setTurn] = useState("O");
-  const [cells, setCells] = useState(Array(9).fill(""));
-  const [winner, setWinner] = useState();
-  const [isDraw, setIsDraw] = useState(false);
-
-  const checkwinner = (arr) => {
-    let combos = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-
-    for (let combo in combos) {
-      if (arr[combos[combo][0]] === arr[combos[combo][1]] && arr[combos[combo][1]] === arr[combos[combo][2]] && arr[combos[combo][0]] !== "") {
-        setWinner(arr[combos[combo][0]]);
-        return;
-      }
-    }
-  };
-
-  const handleClick = (num) => {
-    if (winner || cells[num] !== "") return;
-
-    let arr = [...cells];
-    if (turn === "X") {
-      arr[num] = "X";
-      setTurn("O");
-    } else {
-      arr[num] = "O";
-      setTurn("X");
-    }
-    checkwinner(arr);
-    setCells(arr);
-    if (!arr.includes("") && !winner) {
-      setIsDraw(true);
-    }
-  };
-
-  const Cell = ({ num }) => {
-    const cellValue = cells[num];
-    const cellClassName = cellValue ? `cell cell-${cellValue}` : "cell";
-
-    return (
-      <td className={cellClassName} onClick={() => handleClick(num)}>
-        {cellValue}
-      </td>
-    );
-  };
-
-  const handleReset = () => {
-    setWinner();
-    setIsDraw(false);
-    setCells(Array(9).fill(""));
-  };
+export default function AboutMe() {
 
   return (
-    <div className="container">
-      <div className={`winner ${winner || isDraw ? "show" : ""}`}>
-        {winner ? `Winner is: ${winner}` : isDraw ? "Its a draw" : ""}
+    <>
+      <div className="nav-bar">
+        <ul>
+          <li><a href="#news">News</a></li>
+          <li><a href="#contact">Contact</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a class="active" href="#home">Home</a></li>
+        </ul>
+
       </div>
-      <table>
-        <tbody>
-          <tr>
-            <Cell num={0} />
-            <Cell num={1} />
-            <Cell num={2} />
-          </tr>
-          <tr>
-            <Cell num={3} />
-            <Cell num={4} />
-            <Cell num={5} />
-          </tr>
-          <tr>
-            <Cell num={6} />
-            <Cell num={7} />
-            <Cell num={8} />
-          </tr>
-        </tbody>
-      </table>
-      <button className="reset-button" onClick={handleReset}>
-        Reset
-      </button>
-    </div>
+
+      <div className="body-text">
+        <h1> About me </h1>
+        <p > {aboutMe} </p>
+        <p > {aboutMe} </p>
+        <p > {aboutMe} </p>
+        <p > {aboutMe} </p>
+        <p > {aboutMe} </p>
+
+      </div>
+    </>
+
   );
 }
